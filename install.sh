ddcdd254e13d0c70ec18585fdee94e305a43a64e
Michael Klein <michael.klein.dev@gmail.com>
2018-07-03 15:10:04 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,10 +12,9 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until `.osx` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-chown -R `whoami`:admin /usr/local/bin
-chown -R `whoami`:admin /usr/local/share
-
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+sudo chown -R $(whoami) $(brew --prefix)/*
 
 brew tap caskroom/versions
 brew install caskroom/cask/brew-cask
