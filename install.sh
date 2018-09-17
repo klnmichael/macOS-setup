25f7cb8f66728d0ceea1b38d65f9569b43665254
Michael Klein <michael.klein.dev@gmail.com>
2018-09-17 10:20:38 -0700

    Added sudo timeout instructions

--- install.sh
+++ install.sh
@@ -7,10 +7,11 @@ p() {
 
 p "Installing Homebrew, a few dependencies and applications. Follow the instructions..."
 
-# Ask for the administrator password upfront
+
+# sudo visudo: Default !tty_tickets
 sudo -v
-# Keep-alive: update existing `sudo` time stamp until `.osx` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
@@ -26,7 +27,7 @@ brew install git
 brew install node
 
 brew tap homebrew/php
-brew install php71
+brew install php72
 brew install composer
 
 brew install bash
@@ -43,12 +44,17 @@ xcode-select --intall
 # Apps
 apps=(
 	atom
+	sublime
 	iterm2
 	cyberduck
 	enpass
 	spotify
 	slack
 	google-chrome
+	firefox
+	tableplus
+	adobe-creative-cloud
+	sketch
 )
 
 # Install apps to /Applications
--- setup.sh
+++ setup.sh
@@ -23,9 +23,8 @@ if ! [[ $r =~ ^([yY])$ ]]; then
 fi
 
 
-# Ask for the administrator password upfront
+# sudo visudo: Default !tty_tickets
 sudo -v
-# Keep-alive: update existing `sudo` time stamp until `.osx` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 
